ea9ff0f5e8532e3b442361b5e8c55fb10dce1e11
hciays <jovinangelo@yahoo.fr>
2023-06-19 17:28:54 +0200

    dummy data creation

---
+++ exporter.py
@@ -0,0 +1,6 @@
+def export_batch_size():
+    return 128
+def export_number_channels():
+    return 1
+
+def export_model()
--- train.py
+++ train.py
@@ -17,22 +17,36 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    device = torch.device("cuda")
+    #device = torch.device("cuda")
     root_dir = args.root_dir
 
     # Data
     train_data = CellDataset(root_dir, split="train", transform=train_transform())
     trainloader = torch.utils.data.DataLoader(
-        train_data, batch_size=16, shuffle=True, num_workers=12
+        train_data, batch_size=1, shuffle=True, num_workers=12
     )
     val_data = CellDataset(root_dir, split="val", transform=val_transform())
     valloader = torch.utils.data.DataLoader(
-        val_data, batch_size=16, shuffle=False, num_workers=12
+        val_data, batch_size=1, shuffle=False, num_workers=12
     )
 
     # Initialize the model and trainer
     model = UNet()
-    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=args.num_epochs, precision="16-mixed", benchmark=True)
+    model = UNet()
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        monitor='val_iou',
+        mode='min',
+        dirpath='checkpoints/',
+        filename='best_model',
+        save_top_k=1,
+    )
+
+    trainer = pl.Trainer(accelerator='cpu', devices=1,
+                         max_epochs=args.num_epochs,
+                         precision="16-mixed",
+                         benchmark=True,
+                         callbacks=checkpoint_callback)
+    trainer = pl.Trainer(accelerator='cpu', devices=1, max_epochs=args.num_epochs, precision="16-mixed", benchmark=True)
 
     # Train the model   
     trainer.fit(model, trainloader, valloader)
